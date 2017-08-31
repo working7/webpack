acc3569fe957a297e92e47995ee5b02641417225
lengbin <746992819@qq.com>
2017-08-31 17:22:18 +0800

    remove

--- webpackna/main.js
+++
@@ -1,6 +0,0 @@
-require("./css/css.css");
-var ku = require("./src/ku.js");
-console.log(ku);
-ku.$.each([1,2,3],function(i,index){
-  console.log(i,index)
-})
--- webpackna/src/shi.js
+++
@@ -1,2 +0,0 @@
-var g =require("./ku.js");
-console.log(g.$)
